096e12987cbcd1befa3dfcf8933183d76c95318a
jeniferplacido <jenifer.sdti@gmail.com>
2020-11-16 22:30:01 -0300

    Lista 6

---
+++ Java/aulas/src/Lista6/cliente.java
@@ -0,0 +1,34 @@
+package Lista6;
+
+import java.util.Scanner;
+
+// 1) Crie uma classe cliente e apresente os atributos e métodos referentes esta classe, 
+//em seguida crie um objeto cliente, defina as instancias deste objeto e apresente as informações deste objeto no console.
+
+		
+	
+public class cliente {
+	Scanner leia = new Scanner(System.in);
+
+		
+		public String nome;
+		public String sobrenome;
+		public char genero;
+		public int anoNascimento;
+
+		public cliente (String nome) {
+			this.nome = nome;
+		}
+		
+		public cliente(String nome, String sobrenome) {
+			this.nome= "Jenifer";
+			this.sobrenome="Placido";
+		}
+		public void idade() {
+			this.anoNascimento = 1990;
+			System.out.println(2020-this.anoNascimento);
+
+			
+	
+}
+}
---
+++ Java/aulas/src/Lista6/funcionario.java
@@ -0,0 +1,10 @@
+package Lista6;
+
+public class funcionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
